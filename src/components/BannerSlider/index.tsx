b6a039538bb8cbee6337972a45d1fb6d1c755775
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-09-14 01:11:40 -0300

    isso deve adicionar o sistema de isOpen

--- src/components/BannerSlider/index.tsx
+++ src/components/BannerSlider/index.tsx
@@ -5,36 +5,48 @@ import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined/Arrow
 import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined/ArrowForwardIos'
 
 import * as S from './styles'
+import { useState } from 'react'
 
 export type BannerSliderProps = {
   items: BannerProps[]
 }
 
-const settings: SliderSettings = {
-  arrows: true,
-  infinite: true,
+const BannerSlider = ({ items }: BannerSliderProps) => {
+  // Pegando o slide atual
+  const [activeState, setActiveState] = useState<string>(items[0].title)
 
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        arrows: false
+  function afterChangeHandler(currentSlider: number) {
+    setActiveState(items[currentSlider].title)
+    console.log(activeState)
+  }
+
+  const settings: SliderSettings = {
+    arrows: true,
+    infinite: true,
+    afterChange: (current) => afterChangeHandler(current),
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: false
+        }
       }
-    }
-  ],
+    ],
 
-  nextArrow: <ArrowRight aria-label="next" />,
-  prevArrow: <ArrowLeft aria-label="previous" />
+    nextArrow: <ArrowRight aria-label="next" />,
+    prevArrow: <ArrowLeft aria-label="previous" />
+  }
+
+  return (
+    <S.Wrapper>
+      <Slider settings={settings}>
+        {items.map((item) => (
+          <Banner key={item.title} {...item} isOpen={item.title == activeState} />
+        ))}
+      </Slider>
+    </S.Wrapper>
+  )
 }
-
-const BannerSlider = ({ items }: BannerSliderProps) => (
-  <S.Wrapper>
-    <Slider settings={settings}>
-      {items.map((item) => (
-        <Banner key={item.title} {...item} />
-      ))}
-    </Slider>
-  </S.Wrapper>
-)
 
 export default BannerSlider
--- src/components/BannerSlider/mock.ts
+++ src/components/BannerSlider/mock.ts
@@ -1,6 +1,7 @@
+import { BannerProps } from 'components/Banner'
 import Heading from 'components/Heading'
 
-export default [
+const mock: BannerProps[] = [
   {
     img: 'img/index/Pessoa-Física-votuporanga.jpg',
     title: 'Revisão Tributária',
@@ -8,7 +9,8 @@ export default [
       'Assim, garantindo que as atividades de nossos parceiros estejam de acordo com as exigências legais de seu ramo de atuação. ',
     buttonLabel: 'Veja mais',
     buttonLink: '/',
-    textDirection: 'left'
+    textDirection: 'left',
+    titleImage: 'Um casa planejando as contas'
   },
   {
     img: 'img/index/Fiscal-e-Tributária-votuporanga.jpg',
@@ -18,7 +20,8 @@ export default [
       'Youtubers, Produtores, Coprodutores, Especialistas, Afiliados, Gerente de Afiliados, Eugência, Agência de Lançamentos, Gestor de Tráfego, Redator, Copywriter, Designer Gráfico e Video Maker são como Profissões do Futuro.',
     buttonLabel: 'Veja mais',
     buttonLink: '',
-    textDirection: 'center'
+    textDirection: 'center',
+    titleImage: 'Contadores fazendo as contas'
   },
   {
     img: 'img/index/Contabilidade-Rural-votuporanga-02-scaled.jpg',
@@ -26,6 +29,9 @@ export default [
     subtitle: 'Venha fazer uma parceria de sucesso ingressando ao novo normal.',
     buttonLabel: 'Veja mais',
     buttonLink: '/',
-    textDirection: 'right'
+    textDirection: 'right',
+    titleImage: 'Uma familia sorrindo'
   }
 ]
+
+export default mock
