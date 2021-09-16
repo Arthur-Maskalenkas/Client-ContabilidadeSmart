213f33cf640d62452a6b2cb53e716f9200373349
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-09-16 10:58:01 -0300

    isso deve adicionar um dropdown generico

---
+++ src/components/Dropdown/test.tsx
@@ -0,0 +1,35 @@
+import { fireEvent, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Dropdown, { DropdownProps } from '.'
+
+const props: DropdownProps = { title: 'um titulo', children: 'um filho' }
+
+describe('<Dropdown />', () => {
+  it('vai renderizar o componente', () => {
+    renderWithTheme(<Dropdown {...props} />)
+
+    expect(screen.getByText(/um titulo/i))
+  })
+
+  it('vai abrir e fechar o dropdown quando clica no titulo', () => {
+    renderWithTheme(<Dropdown {...props} />)
+
+    const dropdownElement = screen.getByLabelText(/dropdown/i)
+
+    // Verifica se o drop esta escondido
+    expect(dropdownElement.getAttribute('aria-hidden')).toBe('true')
+    expect(dropdownElement).toHaveStyle({ opacity: 0 })
+
+    // Verifica se caso clicar no titulo o dropdown é aberto
+    userEvent.click(screen.getByText(/um titulo/i))
+    expect(dropdownElement.getAttribute('aria-hidden')).toBe('false')
+    expect(dropdownElement).toHaveStyle({ opacity: 1 })
+
+    // Verifica se caso eu clique no titulo novamente o dropdown é fechado
+    userEvent.click(screen.getByText(/um titulo/i))
+    expect(dropdownElement.getAttribute('aria-hidden')).toBe('true')
+    expect(dropdownElement).toHaveStyle({ opacity: 0 })
+  })
+})
