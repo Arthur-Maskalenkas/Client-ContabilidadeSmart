0a055d7cf5191afb22d595e1f22d1b891747b4e2
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-09-18 10:46:13 -0300

    isso deve implementar testes

--- src/components/DropdownMobile/test.tsx
+++ src/components/DropdownMobile/test.tsx
@@ -1,9 +1,28 @@
+import userEvent from '@testing-library/user-event'
 import { render, screen } from 'utils/test-utils'
 
 import DropdownMobile from '.'
 
+import items from './mock'
+
 describe('<DropdownMobile />', () => {
-  it('should render the heading', () => {
-    render(<DropdownMobile />)
+  it('vai abrir e fechar o dropdown quando clica no titulo', () => {
+    render(<DropdownMobile {...items} />)
+
+    const dropdownElement = screen.getByLabelText(/dropdown/i)
+
+    // Verifica se o drop esta escondido
+    expect(dropdownElement.getAttribute('aria-hidden')).toBe('true')
+    expect(dropdownElement).toHaveStyle({ opacity: 0 })
+
+    // Verifica se caso clicar no titulo o dropdown é aberto
+    userEvent.click(screen.getByText(/Premium/i))
+    expect(dropdownElement.getAttribute('aria-hidden')).toBe('false')
+    expect(dropdownElement).toHaveStyle({ opacity: 1 })
+
+    // Verifica se caso eu clique no titulo novamente o dropdown é fechado
+    userEvent.click(screen.getByText(/Premium/i))
+    expect(dropdownElement.getAttribute('aria-hidden')).toBe('true')
+    expect(dropdownElement).toHaveStyle({ opacity: 0 })
   })
 })
