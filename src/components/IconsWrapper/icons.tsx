8bb7772823141801744c932457ad6474bfec7143
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-09-24 06:39:47 -0300

    icones adicionados

--- src/components/IconsWrapper/icons.tsx
+++ src/components/IconsWrapper/icons.tsx
@@ -1,7 +1,27 @@
 import { Facebook, Whatsapp, Instagram } from '@styled-icons/boxicons-logos'
 
+import {
+  Cubes,
+  Cog,
+  AmericanSignLanguageInterpreting,
+  ChartArea,
+  Coffee,
+  Child
+} from '@styled-icons/fa-solid'
+
 export const IconsDefault = {
   Facebook: <Facebook title="Facebook" role="svg" />,
   Whatsapp: <Whatsapp title="Whatsapp" role="svg" />,
-  Instagram: <Instagram title="Instagram" role="svg" />
+  Instagram: <Instagram title="Instagram" role="svg" />,
+  Cubes: <Cubes title="Cubes" role="svg" />,
+  Cog: <Cog title="Cog" role="svg" />,
+  AmericanSignLanguageInterpreting: (
+    <AmericanSignLanguageInterpreting
+      title="AmericanSignLanguageInterpreting"
+      role="svg"
+    />
+  ),
+  ChartArea: <ChartArea title="ChartArea" role="svg" />,
+  Coffee: <Coffee title="Coffee" role="svg" />,
+  Child: <Child title="Child" role="svg" />
 }
