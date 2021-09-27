3069dce65313e914ce65fe9b9bb7f8577da30e5c
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-09-27 06:55:02 -0300

    projeto com os componentes finalizados, partindo agora para os templates e refatoraçao

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,24 +1,9 @@
-import Gallery, { GalleryProps } from 'components/Gallery'
-
 import Base from 'templates/Base'
 import * as S from './styles'
 
-export type HomeTemplateProps = {
-  gallery: GalleryProps
-}
-
-const Home = ({ gallery }: HomeTemplateProps) => (
+const Home = () => (
   <Base>
-    <S.Wrapper>
-      <S.SectionMain>
-        <Gallery {...gallery} />
-        <S.Intro>
-          <S.Title>Não perca uma noticia!</S.Title>
-          <S.Title>Acompanhe tudo!</S.Title>
-          <S.Title>Faça os outros acompanharem!</S.Title>
-        </S.Intro>
-      </S.SectionMain>
-    </S.Wrapper>
+    <S.Wrapper></S.Wrapper>
   </Base>
 )
 
