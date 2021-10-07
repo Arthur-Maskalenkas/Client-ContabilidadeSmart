9694ad79795deafa1e1b817d3c70bfe3d77db309
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-10-07 09:02:40 -0300

    estilos refatorados, passando para first mobile

--- src/templates/Artigo/styles.ts
+++ src/templates/Artigo/styles.ts
@@ -2,6 +2,8 @@ import { Container } from 'components/Container'
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
+import * as WidgetStyles from 'components/Widget/styles'
+
 export const Wrapper = styled.main``
 
 export const Head = styled.header`
@@ -21,20 +23,44 @@ const Section = styled(Container).attrs({ as: 'section' })`
 `
 
 export const MainSection = styled(Section)`
-  display: grid;
-  grid-template-columns: 1fr 35rem;
-  gap: 0 3rem;
+  display: block;
+
+  ${media.greaterThan('large')`
+    display: grid;
+    grid-template-columns: 1fr 35rem;
+    gap: 0 3rem;
+  `}
 `
 
 export const Main = styled.main`
-  padding: 2rem;
-  box-shadow: 0px 0px 24px -16px #000000;
+  ${({ theme }) => css`
+    padding: 2rem;
+    height: max-content;
 
-  height: max-content;
+    margin-left: -${theme.grid.gutter};
+    margin-right: -${theme.grid.gutter};
+
+    ${media.greaterThan('large')`
+      margin-left: ${theme.grid.gutter};
+      margin-right: ${theme.grid.gutter};
+
+      box-shadow: 0px 0px 24px -16px #000000;
+  `}
+  `}
 `
 
 export const SectionWidgets = styled(Section)`
-  display: grid;
-  grid-template-columns: 26rem 26rem 26rem 1fr;
-  gap: 3rem;
+  ${({ theme }) => css`
+    display: block;
+
+    ${WidgetStyles.Wrapper} {
+      margin-bottom: ${theme.spacings.medium};
+    }
+
+    ${media.greaterThan('large')`
+      display: grid;
+      grid-template-columns: 26rem 26rem 26rem 1fr;
+      gap: 3rem;
+  `}
+  `}
 `
