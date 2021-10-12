7ad340029fadd05714f4b6b030622381da70b1af
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-10-12 10:20:09 -0300

    correçao no bug de array duplo gerado na index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,40 @@
 import Home, { HomeTemplateProps } from 'templates/Home'
 
 import mockBannerSlider from 'components/BannerSlider/mock'
-import mockMenu from 'components/Menu/mock'
 import mockWidgetList from 'components/WidgetList/mock'
+
+import { initializeApollo } from 'utils/apollo'
+import { QueryMenu } from 'graphql/generated/QueryMenu'
+import { QUERY_MENU } from 'graphql/queries/menu'
 
 export default function sobre(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const {
+    data: { menus }
+  } = await apolloClient.query<QueryMenu>({
+    query: QUERY_MENU,
+    fetchPolicy: 'no-cache'
+  })
+
   return {
     props: {
       bannerSliderData: mockBannerSlider,
       widgetListData: mockWidgetList,
-      menuData: mockMenu
+      menuData: {
+        items: menus.map((item) => ({
+          title: item.title,
+          slug: item.slug,
+          dropdownOptions: item.menu_options.map((item) => ({
+            titleOption: item.title,
+            slug: item.slug
+          }))
+        }))
+      }
     }
   }
 }
