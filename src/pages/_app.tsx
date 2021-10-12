11003c04803f8596b036bf56cc6e6eec6eac5ce4
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-10-12 03:58:18 -0300

    apollo client configurado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,36 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+
+import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
+import { useApollo } from 'utils/apollo'
+
 function App({ Component, pageProps }: AppProps) {
+  const client = useApollo(pageProps.initialApolloState)
+
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Head>
-          <title>React Avançado - Boilerplate</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#06092B" />
-          <meta
-            name="description"
-            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-          />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <Head>
+            <title>React Avançado - Boilerplate</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="theme-color" content="#06092B" />
+            <meta
+              name="description"
+              content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+            />
+          </Head>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ApolloProvider>
     </>
   )
 }
