f2bbc109347cb57e1e668ac9842f626fd735e3df
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-10-14 18:10:11 -0300

    data formatada

--- src/components/BannerPage/index.tsx
+++ src/components/BannerPage/index.tsx
@@ -6,13 +6,16 @@ export type BannerPageProps = {
   tag: string
 }
 
-const BannerPage = ({ data, tag, backgroundImage }: BannerPageProps) => (
+const BannerPage = ({ data, tag, backgroundImage }: BannerPageProps) => {
+  const dataFormatada = data.replace(/(\d*)-(\d*)-(\d*).*/, '$3-$2-$1').replace('on','')
+
+  return (
   <S.Wrapper backgroundImage={backgroundImage}>
     <S.Overlay />
     <S.DetailsWrapper>
-      <S.Details>{`on ${data} | By Contabilidade Smart | ${tag}`}</S.Details>
+      <S.Details>{`${dataFormatada} | By Contabilidade Smart | ${tag}`}</S.Details>
     </S.DetailsWrapper>
   </S.Wrapper>
-)
+)}
 
 export default BannerPage
