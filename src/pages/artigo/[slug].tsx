dad48f0891e3f46633e87bd8c6caa2b362acf959
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-10-14 10:45:18 -0300

    paths gerados dinamicamente

--- src/pages/artigo/[slug].tsx
+++ src/pages/artigo/[slug].tsx
@@ -14,6 +14,13 @@ import { QueryWidgets } from 'graphql/generated/QueryWidgets'
 import { QueryTags } from 'graphql/generated/QueryTags'
 
 import { QUERY_MENUASIDE } from 'graphql/queries/menuAside'
+import { QueryPosts, QueryPostsVariables } from 'graphql/generated/QueryPosts'
+import { QUERY_POSTS } from 'graphql/queries/posts'
+
+import { GetStaticProps } from 'next'
+
+// Inicializando por fora para usar no get e no server
+const apolloClient = initializeApollo();
 
 export default function Index(props: ArtigoTemplateProps) {
   const router = useRouter()
@@ -25,15 +32,19 @@ export default function Index(props: ArtigoTemplateProps) {
 }
 
 export async function getStaticPaths() {
-  return {
-    paths: [{ params: { slug: 'MEI' } }],
-    fallback: true // Se não tiver sido feita em build time, ele vai gerar a pagina de erro
-  }
+  const { data } = await apolloClient.query<QueryPosts, QueryPostsVariables>({query: QUERY_POSTS, variables: {limit: 9}})
+
+  const paths = data.posts.map(({slug}) => ({
+    params: { slug }
+  }))
+
+  // Caso a pagina não exista, ele vai correr atras (fallback)
+  return { paths, fallback: true }
 }
 
 const texto = `<body contenteditable="true" class="cke_editable cke_editable_themed cke_contents_ltr cke_show_borders" spellcheck="false"><address><p>Hoje em dia é possível aumentar a renda com produtos de venda direta além dos perfumes, cosméticos e produtos de alimentação? Sim.</p><p>Se antes você tinha que andar com uma mochila cargueira cheia de produtos e vender porta-a-porta agora é Net-a-net.</p><p>Hoje no Brasil e no mundo com o novo normal, estamos acompanhando a crescente profissionalização do mercado de venda direta, as grandes empresas brasileiras já estão atuando como multinacionais ou em início de expansão, esse passo foi dado ainda na década de 70 no mercado norte americano.</p><p>Veja os próximos passos no mercado brasileiro que sem dúvida vamos ter nos próximos anos com o novo normal:</p><ol><li>Uso da Internet tanto na venda direta, como prospecção de novos consumidores;</li><li>Novos produtos e linhas diferentes das tradicionais;</li><li>A procura de novas ferramentas e treinamentos online;</li></ol><p>Milhares de leitores tem ideia da importância crescente da internet, utilizando celulares smartphones das marcas Xiaomi, Iphone e Samsung como ferramenta e não um fim, seja para aprendizado ou para localizar e expandir novas listas de prospects.</p><p><br></p><p><strong>Veja a baixo anúncios da Avon nas décadas de 1940 e 1950</strong></p><p><span tabindex="-1" contenteditable="false" data-cke-widget-wrapper="1" data-cke-filter="off" class="cke_widget_wrapper cke_widget_inline cke_image_nocaption" data-cke-display-name="Imagem" data-ck`
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const apolloClient = initializeApollo()
 
     //  Menuaside
