330968af2a591f89be67fe3b7d6aa58c81634f90
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-10-14 09:29:45 -0300

    query de tags criada

---
+++ src/graphql/generated/QueryTags.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: QueryTags
+// ====================================================
+
+export interface QueryTags_tags {
+  __typename: "Tag";
+  Title: string;
+  slug: string;
+}
+
+export interface QueryTags {
+  tags: QueryTags_tags[];
+}
---
+++ src/graphql/queries/menuAside.ts
@@ -0,0 +1,10 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_MENUASIDE = gql`
+  query QueryTags {
+   tags {
+      Title
+      slug
+    }
+}
+`
