97d65db6f2866d08377a439f9253173dc18c110e
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-10-15 20:03:09 -0300

    fix

--- src/components/MaisVistos/stories.tsx
+++ src/components/MaisVistos/stories.tsx
@@ -12,21 +12,8 @@ export default {
 
 export const Default: Story<MaisVistosProps> = (args) => <MaisVistos {...args} />
 
-const mockWithItems =
-{ items:
-  [
-    {
-      title: 'Quais os tipos de lançamentos', slug: 'quais_os_tipos_de_lançamentos'
-    },
-    {
-      title: 'Cansado!? 14 dicas faceis para criar o seu infoproduto',
-      slug: 'cansado?!_14_dicas_faceis_para_criar_o_seu_infoproduto'
-    }
-  ]
-}
 
-
-Default.args = { ...mockWithItems }
+Default.args = { items: mock }
 
 Default.parameters = {
   backgrounds: {
@@ -36,7 +23,7 @@ Default.parameters = {
 
 export const Mobile: Story<MaisVistosProps> = (args) => <MaisVistos {...args} />
 
-Mobile.args = { ...mockWithItems }
+Mobile.args = { items: mock  }
 
 Mobile.parameters = {
   backgrounds: {
