2bfd4a5dc5bdc484aba1b453fde96a2090f98659
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-10-16 10:16:24 -0300

    template de buscar montado

--- src/templates/Buscar/index.tsx
+++ src/templates/Buscar/index.tsx
@@ -1,7 +1,11 @@
-import { BannerPageProps } from 'components/BannerPage'
+import BannerPage, { BannerPageProps } from 'components/BannerPage'
+import HeadingPage from 'components/HeadingPage'
 import { MenuUnitaryProps } from 'components/Menu'
-import { MenuAsideProps } from 'components/MenuAside'
+import MenuAside, { MenuAsideProps } from 'components/MenuAside'
+import TextContent from 'components/TextContent'
 import { WidgetProps } from 'components/Widget'
+import WidgetList from 'components/WidgetList'
+import Base from 'templates/Base'
 
 import * as S from './styles'
 
@@ -19,10 +23,43 @@ export type BuscarTemplateProps = {
   menuAsideItems: MenuAsideProps
 }
 
-const Buscar = () => (
-  <S.Wrapper>
-    <h1>Buscar</h1>
-  </S.Wrapper>
-)
+const Buscar = ({
+  title,
+  description,
+  bannerPageProps,
+  widgetListCategoriasData,
+  widgetListPaginasData,
+  widgetPostsRecentes,
+  menuAsideItems,
+  menuData
+}: BuscarTemplateProps) => {
+  const widgets = [
+    ...widgetListCategoriasData,
+    ...widgetListPaginasData,
+    ...widgetPostsRecentes
+  ]
+
+  return (
+    <S.Wrapper>
+      <Base menuData={menuData}>
+        <S.Head>
+          <HeadingPage title={title} />
+        </S.Head>
+
+        <S.MainSection>
+          <S.Main>
+            <BannerPage {...bannerPageProps} />
+            <TextContent title={title} content={description} />
+          </S.Main>
+          <MenuAside {...menuAsideItems} />
+        </S.MainSection>
+
+        <S.WidgetSection>
+          <WidgetList items={widgets} />
+        </S.WidgetSection>
+      </Base>
+    </S.Wrapper>
+  )
+}
 
 export default Buscar
--- src/templates/Buscar/styles.ts
+++ src/templates/Buscar/styles.ts
@@ -1,3 +1,56 @@
-import styled from 'styled-components'
+import { Container } from 'components/Container'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+import * as BannerStyles from 'components/BannerPage/styles'
 
 export const Wrapper = styled.main``
+
+export const Head = styled.header`
+  ${({ theme }) => css`
+    margin-bottom: ${theme.spacings.large};
+  `}
+`
+
+const Section = styled(Container).attrs({ as: 'section' })`
+  ${({ theme }) => css`
+    margin-bottom: ${theme.spacings.xlarge};
+
+    ${media.greaterThan('medium')`
+      margin-bottom: calc(${theme.spacings.xlarge} * 2)
+    `}
+  `}
+`
+
+export const MainSection = styled(Section)`
+  display: block;
+
+  ${media.greaterThan('large')`
+    display: grid;
+    grid-template-columns: 1fr 35rem;
+    gap: 0 3rem;
+  `}
+`
+
+export const Main = styled.main`
+  ${({ theme }) => css`
+    padding: 2rem;
+    height: max-content;
+
+    margin-left: -${theme.grid.gutter};
+    margin-right: -${theme.grid.gutter};
+
+    ${media.greaterThan('large')`
+      margin-left: ${theme.grid.gutter};
+      margin-right: ${theme.grid.gutter};
+
+      box-shadow: 0px 0px 24px -16px #000000;
+  `}
+
+    ${BannerStyles.Wrapper} {
+      margin-bottom: ${theme.spacings.large};
+    }
+  `}
+`
+
+export const WidgetSection = styled(Section)``
