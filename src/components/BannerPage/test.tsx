0e54e6016925667713bdebdcec63471ca727cfa1
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-10-16 09:18:24 -0300

    refact

--- src/components/BannerPage/test.tsx
+++ src/components/BannerPage/test.tsx
@@ -1,19 +1,22 @@
 import { render, screen } from 'utils/test-utils'
 
-import BannerPage from '.'
+import BannerPage, { BannerPageProps } from '.'
 
-const props = {
+const props: BannerPageProps = {
   backgroundImage: 'backgroundUrl',
   data: '23 out 2020',
-  tag: 'Noticias'
+  tag: 'Noticias',
+  altImage: 'uma imagem'
 }
 
 describe('<BannerPage />', () => {
-  it('should render the heading', () => {
+  it('Vai renderizar o banner', () => {
     render(<BannerPage {...props} />)
 
     expect(
       screen.getByText(/on 23 out 2020 | by Contabilidade smart | Noticias/i)
     )
+
+    expect(screen.getByAltText(/uma image/i)).toBeInTheDocument()
   })
 })
