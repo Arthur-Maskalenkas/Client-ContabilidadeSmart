6a2fbd05817ea19e209abfe2d286ab595ec79e88
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-10-17 11:22:27 -0300

    mappers terminados e testados

--- src/utils/mappers/index.ts
+++ src/utils/mappers/index.ts
@@ -1,4 +1,10 @@
+import { QueryBannersHome_home_bannerHome } from 'graphql/generated/QueryBannersHome'
 import { QueryMenu_menus } from 'graphql/generated/QueryMenu'
+import {
+  QueryWidgets_widgetsCategorias,
+  QueryWidgets_widgetsPaginas,
+  QueryWidgets_widgetsPostsRecentes
+} from 'graphql/generated/QueryWidgets'
 
 export const menuMapper = (menu: QueryMenu_menus[]) => {
   return menu
@@ -9,6 +15,71 @@ export const menuMapper = (menu: QueryMenu_menus[]) => {
           titleOption: item.title,
           slug: item?.post?.slug
         }))
+      }))
+    : []
+}
+
+export const widgetCategoriasMapper = (
+  widgetCategorias: QueryWidgets_widgetsCategorias[]
+) => {
+  return widgetCategorias
+    ? widgetCategorias.map((item) => ({
+        title: item.title,
+        path: item.path,
+        items: item.categorias.map((item) => ({
+          title: item.title,
+          slug: item.slug
+        }))
+      }))
+    : []
+}
+
+export const widgetPaginasMapper = (
+  widgetCategorias: QueryWidgets_widgetsPaginas[]
+) => {
+  return widgetCategorias
+    ? widgetCategorias.map((item) => ({
+        title: item.title,
+        path: item.path,
+        items: item.posts.map((item) => ({
+          title: item.title,
+          slug: item.slug
+        }))
+      }))
+    : []
+}
+
+export const widgetPostsRecentesMapper = (
+  widgetPostsRecentes: QueryWidgets_widgetsPostsRecentes[]
+) => {
+  return widgetPostsRecentes
+    ? [
+        {
+          moreWeight: true,
+          title: 'Posts recentes',
+          path: 'posts_recentes',
+          items: widgetPostsRecentes.map((item) => ({
+            title: item.title,
+            path: item.slug
+          }))
+        }
+      ]
+    : []
+}
+
+export const bannerSliderMapper = (
+  bannerSlider: QueryBannersHome_home_bannerHome[] | null
+) => {
+  return bannerSlider
+    ? bannerSlider?.map((item) => ({
+        title: item?.title,
+        subtitle: item?.subtitle,
+        textDirection: item?.textDirection,
+        titleWithColor: item?.titleWithColor,
+        img: item?.img?.url,
+        titleImage: item?.img?.alternativeText,
+        buttonLabel: item?.buttonLabel,
+        buttonLink: item?.buttonLink
       }))
     : []
 }
--- src/utils/mappers/test.ts
+++ src/utils/mappers/test.ts
@@ -1,16 +1,32 @@
-import { menuMapper } from '.'
+import {
+  bannerSliderMapper,
+  menuMapper,
+  widgetCategoriasMapper,
+  widgetPaginasMapper,
+  widgetPostsRecentesMapper
+} from '.'
 
 import { QueryMenu_menus } from 'graphql/generated/QueryMenu'
+
 import {
   QueryWidgets_widgetsCategorias,
   QueryWidgets_widgetsPaginas,
   QueryWidgets_widgetsPostsRecentes
 } from 'graphql/generated/QueryWidgets'
-import { ENUM_WIDGET_PATH } from 'graphql/generated/globalTypes'
+
+import {
+  ENUM_WIDGET_PATH,
+  ENUM_COMPONENTBANNERBANNER_TEXTDIRECTION
+} from 'graphql/generated/globalTypes'
+
+import { WidgetProps } from 'components/Widget'
+import { MenuUnitaryProps } from 'components/Menu'
+import { QueryBannersHome_home_bannerHome } from 'graphql/generated/QueryBannersHome'
+import { BannerProps } from 'components/Banner'
 
 describe('menuMapper()', () => {
-  it('retorna o valor certo completamente mapeado', () => {
-    const menu: QueryMenu_menus[] = [
+  it('retorna o valor certo completamente mapeado de um menu', () => {
+    const valueApi: QueryMenu_menus[] = [
       {
         __typename: 'Menu',
         title: 'um titulo do menu',
@@ -25,24 +41,24 @@ describe('menuMapper()', () => {
       }
     ]
 
-    expect(menuMapper(menu)).toStrictEqual([
-      {
-        title: 'um titulo do menu',
-        slug: 'um slug do menu',
-        dropdownOptions: [
-          {
-            titleOption: 'um titulo do dropdown',
-            slug: 'um slug do post do dropdown'
-          }
-        ]
-      }
-    ])
+    const expectValue: MenuUnitaryProps = {
+      title: 'um titulo do menu',
+      slug: 'um slug do menu',
+      dropdownOptions: [
+        {
+          titleOption: 'um titulo do dropdown',
+          slug: 'um slug do post do dropdown'
+        }
+      ]
+    }
+
+    expect(menuMapper(valueApi)).toStrictEqual([expectValue])
   })
 })
 
-describe('widgetsMapperCategoria()', () => {
-  it('retorna o valor certo completamente mapeado', () => {
-    const menu: QueryWidgets_widgetsCategorias[] = [
+describe('widgetsCategoriasMapper()', () => {
+  it('retorna o valor certo completamente mapeado de um widget de categorias', () => {
+    const valueApi: QueryWidgets_widgetsCategorias[] = [
       {
         __typename: 'Widget',
         path: ENUM_WIDGET_PATH.categorias,
@@ -56,15 +72,23 @@ describe('widgetsMapperCategoria()', () => {
         ]
       }
     ]
+
+    const expectValue: WidgetProps = {
+      title: 'Veja uma noticia',
+      path: 'categorias',
+      items: [{ title: 'uma noticia', slug: 'slug de uma noticia' }]
+    }
+
+    expect(widgetCategoriasMapper(valueApi)).toStrictEqual([expectValue])
   })
 })
 
-describe('widgetsMapperCategoria()', () => {
-  it('retorna o valor certo completamente mapeado', () => {
-    const menu: QueryWidgets_widgetsPaginas[] = [
+describe('widgetsPaginasMapper()', () => {
+  it('retorna o valor certo completamente mapeado de um widget de Paginas', () => {
+    const valueApi: QueryWidgets_widgetsPaginas[] = [
       {
         __typename: 'Widget',
-        path: ENUM_WIDGET_PATH.categorias,
+        path: ENUM_WIDGET_PATH.paginas,
         title: 'Veja um post',
         posts: [
           {
@@ -75,20 +99,74 @@ describe('widgetsMapperCategoria()', () => {
         ]
       }
     ]
+
+    const expectValue: WidgetProps = {
+      title: 'Veja um post',
+      path: 'paginas',
+      items: [{ title: 'um post', slug: 'slug de um post' }]
+    }
+
+    expect(widgetPaginasMapper(valueApi)).toStrictEqual([expectValue])
   })
 })
 
-// fix
-describe('widgetsMapperPaginas()', () => {
-  it('retorna o valor certo completamente mapeado', () => {
-    const menu: QueryWidgets_widgetsPostsRecentes[] = [
+// Esse vai retornar o map apenas de post, ja que o pai vai ser alterado pelo codigo
+describe('widgetsPostsRecentesMapper()', () => {
+  it('retorna o valor certo completamente mapeado de um widget de posts recentes', () => {
+    const valueApi: QueryWidgets_widgetsPostsRecentes[] = [
       {
         __typename: 'Post',
-            title: 'um post',
-            slug: 'slug de um post'
-          }
-        ]
+        title: 'um titulo de um post recente',
+        slug: 'posts_recentes'
       }
     ]
+
+    const expectValue = {
+      moreWeight: true,
+      title: 'Posts recentes',
+      path: 'posts_recentes',
+      items: [
+        {
+          title: 'um titulo de um post recente',
+          path: 'posts_recentes'
+        }
+      ]
+    }
+
+    expect(widgetPostsRecentesMapper(valueApi)).toStrictEqual([expectValue])
+  })
+})
+
+describe('bannerSliderMapper()', () => {
+  it('retorna o valor completamente mapeado de um bannerSlider', () => {
+    const valueApi: QueryBannersHome_home_bannerHome[] = [
+      {
+        __typename: 'ComponentBannerBanner',
+        title: 'titulo de um banner',
+        subtitle: 'subtitulo de um banner',
+        titleWithColor: 'texto com cor de um banner',
+        textDirection: ENUM_COMPONENTBANNERBANNER_TEXTDIRECTION.right,
+        buttonLabel: 'o label de um botao',
+        buttonLink: 'www.google.com.br',
+        img: {
+          __typename: 'UploadFile',
+          url: 'uma url',
+          alternativeText: 'um alt'
+        }
+      }
+    ]
+
+    const expectValue: BannerProps = {
+      title: 'titulo de um banner',
+      subtitle: 'subtitulo de um banner',
+      titleWithColor: 'texto com cor de um banner',
+      textDirection: 'right',
+      buttonLabel: 'o label de um botao',
+      buttonLink: 'www.google.com.br',
+      titleImage: 'um alt',
+      img: 'uma url'
+    }
+
+    expect(bannerSliderMapper(valueApi)).toStrictEqual([expectValue])
   })
 })
