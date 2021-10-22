d2e705f022900940f01911c726dcf2e57d70b6ad
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-10-22 19:28:20 -0300

    adicionada condicional de posts em tags

--- src/components/Tags/index.tsx
+++ src/components/Tags/index.tsx
@@ -2,29 +2,43 @@ import Link from 'next/link'
 
 import * as S from './styles'
 
+type PostType = {
+  id: string
+}
+
 export type TagProps = {
   title: string
   slug: string
+  posts: PostType[]
 }
 
 export type TagsProps = {
   items?: TagProps[]
 }
 
-const Tags = ({ items }: TagsProps) => (
-  <S.Wrapper>
-    {items?.map((item, index) => (
-      <S.TagWrapper
-        key={index}
-        role="listitem"
-        aria-label={`Opção de tag ${item.title}`}
-      >
-        <Link passHref href={`/buscar?tags=${item.slug}`}>
-          <S.TagLink>{item.title}</S.TagLink>
-        </Link>
-      </S.TagWrapper>
-    ))}
-  </S.Wrapper>
-)
+const Tags = ({ items }: TagsProps) => {
+  const tagHavePosts = (tag: TagProps) => {
+    return !!tag.posts.length
+  }
+
+  return (
+    <S.Wrapper>
+      {items?.map(
+        (item, index) =>
+          tagHavePosts(item) && (
+            <S.TagWrapper
+              key={index}
+              role="listitem"
+              aria-label={`Opção de tag ${item.title}`}
+            >
+              <Link passHref href={`/buscar?tags=${item.slug}`}>
+                <S.TagLink>{item.title}</S.TagLink>
+              </Link>
+            </S.TagWrapper>
+          )
+      )}
+    </S.Wrapper>
+  )
+}
 
 export default Tags
