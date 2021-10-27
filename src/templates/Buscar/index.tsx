d1c0c294340433fde25add3085f6a14067b12481
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-10-27 07:26:54 -0300

    template buscar refatorado

--- src/templates/Buscar/index.tsx
+++ src/templates/Buscar/index.tsx
@@ -1,20 +1,18 @@
 import { BannerPageProps } from 'components/BannerPage'
-import HeadingPage from 'components/HeadingPage'
 import { MenuUnitaryProps } from 'components/Menu'
-import MenuAside, { MenuAsideProps } from 'components/MenuAside'
-import BuscarPost from 'components/BuscarPost'
+import { MenuAsideProps } from 'components/MenuAside'
 import { WidgetProps } from 'components/Widget'
-import WidgetList from 'components/WidgetList'
 import { useQueryPosts } from 'graphql/queries/posts'
 import { useRouter } from 'next/dist/client/router'
 import { useState } from 'react'
-import Base from 'templates/Base'
 import { parseQueryStringToWhere } from 'utils/filter'
 import { resolvePagination } from 'utils/resolvePagination'
 
 import * as S from './styles'
-import BuscarButton from 'components/BuscarButton'
+import BaseUpgraded from 'templates/BaseUpgraded'
 import BuscarPostLoading from 'components/BuscarPostLoading'
+import BuscarButton from 'components/BuscarButton'
+import BuscarPost from 'components/BuscarPost'
 
 export const POSTS_PER_PAGE = 3
 
@@ -81,53 +79,44 @@ const BuscarTemplate = ({
 
   return (
     <S.Wrapper>
-      <Base menuData={menuData}>
-        <S.Head>
-          <HeadingPage
-            title={`${title.tipoDeBusca} > ${title.parametroDeBusca}`}
+      <BaseUpgraded
+        menuAsideItems={menuAsideItems}
+        menuData={menuData}
+        title={title}
+        widgets={widgets}
+      >
+        {postsSlice.map((item, index) => (
+          <BuscarPost
+            key={index}
+            title={item.title}
+            subtitle={item.subtitulo!}
+            BannerPageProps={{
+              data: item.created_at,
+              altImage: item.capa?.alternativeText || 'img not found',
+              backgroundImage: item.capa?.url,
+              slug: item.slug
+            }}
           />
-        </S.Head>
-        <S.MainSection>
-          <S.Main>
-            {postsSlice.map((item, index) => (
-              <BuscarPost
-                key={index}
-                title={item.title}
-                subtitle={item.subtitulo!}
-                BannerPageProps={{
-                  data: item.created_at,
-                  altImage: item.capa?.alternativeText || 'img not found',
-                  backgroundImage: item.capa?.url,
-                  slug: item.slug
-                }}
-              />
-            ))}
-            {loading ? (
-              <BuscarPostLoading items={3} />
-            ) : (
-              <S.ButtonsWrapper>
-                <BuscarButton
-                  onClick={() => handleClick('nextPage')}
-                  icon="NavigateNext"
-                  currentPage={currentPage}
-                  lastPage={lastPage}
-                />
-                <BuscarButton
-                  onClick={() => handleClick('backPage')}
-                  icon="NavigateBefore"
-                  currentPage={currentPage}
-                  lastPage={lastPage}
-                />
-              </S.ButtonsWrapper>
-            )}
-          </S.Main>
-          <MenuAside {...menuAsideItems} />
-        </S.MainSection>
-
-        <S.WidgetSection>
-          <WidgetList items={widgets} />
-        </S.WidgetSection>
-      </Base>
+        ))}
+        {loading ? (
+          <BuscarPostLoading items={3} />
+        ) : (
+          <S.ButtonsWrapper>
+            <BuscarButton
+              onClick={() => handleClick('nextPage')}
+              icon="NavigateNext"
+              currentPage={currentPage}
+              lastPage={lastPage}
+            />
+            <BuscarButton
+              onClick={() => handleClick('backPage')}
+              icon="NavigateBefore"
+              currentPage={currentPage}
+              lastPage={lastPage}
+            />
+          </S.ButtonsWrapper>
+        )}
+      </BaseUpgraded>
     </S.Wrapper>
   )
 }
