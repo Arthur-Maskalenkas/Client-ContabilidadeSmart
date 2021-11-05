d29e53e5298ca53f4a683fd79ed0136278b19acf
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-11-05 00:16:06 -0300

    testes do componente compartilhe refatorados

--- src/components/Compartilhe/test.tsx
+++ src/components/Compartilhe/test.tsx
@@ -1,16 +1,25 @@
 import { Provider } from 'react-redux'
-import { rootReducer } from 'store/store'
-import { API_WHATSAPP, LINK_SITE } from 'utils/globalVars'
+import {
+  API_FACEBOOK,
+  API_LINKEDIN,
+  API_WHATSAPP,
+  LINK_SITE
+} from 'utils/globalVars'
 import { render, screen } from 'utils/test-utils'
 
 import Compartilhe from '.'
-import { paginaSliceReducer } from './mock'
+import {
+  storePaginaArtigoMock,
+  storePaginaCategoriasMock,
+  storePaginaHomeMock,
+  storePaginaTagsMock
+} from './mock'
 
 describe('<Compartilhe />', () => {
   it('vai renderizar o componente', () => {
     render(
-      <Provider store={rootReducer}>
-        <Compartilhe pageType="home" />
+      <Provider store={storePaginaHomeMock}>
+        <Compartilhe />
       </Provider>
     )
 
@@ -30,8 +39,8 @@ describe('<Compartilhe />', () => {
   describe('Whatsapp Icon', () => {
     it('Vai renderizar um link para home no icone do Whatsapp', () => {
       render(
-        <Provider store={rootReducer}>
-          <Compartilhe pageType="home" />
+        <Provider store={storePaginaHomeMock}>
+          <Compartilhe />
         </Provider>
       )
 
@@ -45,8 +54,8 @@ describe('<Compartilhe />', () => {
 
     it('Vai renderizar um link para categorias no icone do Whatsapp', () => {
       render(
-        <Provider store={paginaSliceReducer}>
-          <Compartilhe pageType="categorias" slug="teste" />
+        <Provider store={storePaginaCategoriasMock}>
+          <Compartilhe slug="teste" />
         </Provider>
       )
 
@@ -57,120 +66,159 @@ describe('<Compartilhe />', () => {
         `${API_WHATSAPP}${LINK_SITE}/buscar?categorias=teste`
       )
     })
+
+    it('Vai renderizar um link para tags no icone do Whatsapp', () => {
+      render(
+        <Provider store={storePaginaTagsMock}>
+          <Compartilhe slug="teste" />
+        </Provider>
+      )
+
+      const whatsappIcon = screen.getByLabelText(/compartilhar no Whatsapp/i)
+
+      expect(whatsappIcon).toHaveAttribute(
+        'href',
+        `${API_WHATSAPP}${LINK_SITE}/buscar?tags=teste`
+      )
+    })
+
+    it('Vai renderizar um link para artigo no icone do Whatsapp', () => {
+      render(
+        <Provider store={storePaginaArtigoMock}>
+          <Compartilhe slug="teste" />
+        </Provider>
+      )
+
+      const whatsappIcon = screen.getByLabelText(/compartilhar no Whatsapp/i)
+
+      expect(whatsappIcon).toHaveAttribute(
+        'href',
+        `${API_WHATSAPP}${LINK_SITE}/artigo/teste`
+      )
+    })
   })
 
-  //   it('Vai renderizar um link para tags no icone do Whatsapp', () => {
-  //     render(<Compartilhe pageType="tags" slug="teste" />)
+  describe('Facebook icon', () => {
+    it('Vai renderizar um link para home no icone do Facebook', () => {
+      render(
+        <Provider store={storePaginaHomeMock}>
+          <Compartilhe />
+        </Provider>
+      )
 
-  //     const whatsappIcon = screen.getByLabelText(/compartilhar no Whatsapp/i)
+      const FacebookIcon = screen.getByLabelText(/compartilhar no Facebook/i)
 
-  //     expect(whatsappIcon).toHaveAttribute(
-  //       'href',
-  //       `${API_WHATSAPP}${LINK_SITE}/buscar?tags=teste`
-  //     )
-  //   })
+      expect(FacebookIcon).toHaveAttribute(
+        'href',
+        `${API_FACEBOOK}${LINK_SITE}`
+      )
+    })
 
-  //   it('Vai renderizar um link para artigo no icone do Whatsapp', () => {
-  //     render(<Compartilhe pageType="artigo" slug="teste" />)
+    it('Vai renderizar um link para categorias no icone do Facebook', () => {
+      render(
+        <Provider store={storePaginaCategoriasMock}>
+          <Compartilhe slug="teste" />
+        </Provider>
+      )
 
-  //     const whatsappIcon = screen.getByLabelText(/compartilhar no Whatsapp/i)
+      const FacebookIcon = screen.getByLabelText(/compartilhar no Facebook/i)
 
-  //     expect(whatsappIcon).toHaveAttribute(
-  //       'href',
-  //       `${API_WHATSAPP}${LINK_SITE}/artigo/teste`
-  //     )
-  //   })
-  // })
+      expect(FacebookIcon).toHaveAttribute(
+        'href',
+        `${API_FACEBOOK}${LINK_SITE}/buscar?categorias=teste`
+      )
+    })
 
-  // describe('Facebook icon', () => {
-  //   it('Vai renderizar um link para home no icone do Facebook', () => {
-  //     render(<Compartilhe pageType="home" />)
+    it('Vai renderizar um link para tags no icone do Facebook', () => {
+      render(
+        <Provider store={storePaginaTagsMock}>
+          <Compartilhe slug="teste" />
+        </Provider>
+      )
 
-  //     const FacebookIcon = screen.getByLabelText(/compartilhar no Facebook/i)
+      const FacebookIcon = screen.getByLabelText(/compartilhar no Facebook/i)
 
-  //     expect(FacebookIcon).toHaveAttribute(
-  //       'href',
-  //       `${API_FACEBOOK}${LINK_SITE}`
-  //     )
-  //   })
+      expect(FacebookIcon).toHaveAttribute(
+        'href',
+        `${API_FACEBOOK}${LINK_SITE}/buscar?tags=teste`
+      )
+    })
 
-  //   it('Vai renderizar um link para categorias no icone do Facebook', () => {
-  //     render(<Compartilhe pageType="categorias" slug="teste" />)
+    it('Vai renderizar um link para artigo no icone do Facebook', () => {
+      render(
+        <Provider store={storePaginaArtigoMock}>
+          <Compartilhe slug="teste" />
+        </Provider>
+      )
 
-  //     const FacebookIcon = screen.getByLabelText(/compartilhar no Facebook/i)
+      const FacebookIcon = screen.getByLabelText(/compartilhar no Facebook/i)
 
-  //     expect(FacebookIcon).toHaveAttribute(
-  //       'href',
-  //       `${API_FACEBOOK}${LINK_SITE}/buscar?categorias=teste`
-  //     )
-  //   })
+      expect(FacebookIcon).toHaveAttribute(
+        'href',
+        `${API_FACEBOOK}${LINK_SITE}/artigo/teste`
+      )
+    })
+  })
 
-  //   it('Vai renderizar um link para tags no icone do Facebook', () => {
-  //     render(<Compartilhe pageType="tags" slug="teste" />)
+  describe('Linkedin icon', () => {
+    it('Vai renderizar um link para home no icone do Linkedin', () => {
+      render(
+        <Provider store={storePaginaHomeMock}>
+          <Compartilhe />
+        </Provider>
+      )
 
-  //     const FacebookIcon = screen.getByLabelText(/compartilhar no Facebook/i)
+      const LinkedinIcon = screen.getByLabelText(/compartilhar no Linkedin/i)
 
-  //     expect(FacebookIcon).toHaveAttribute(
-  //       'href',
-  //       `${API_FACEBOOK}${LINK_SITE}/buscar?tags=teste`
-  //     )
-  //   })
+      expect(LinkedinIcon).toHaveAttribute(
+        'href',
+        `${API_LINKEDIN}${LINK_SITE}`
+      )
+    })
 
-  //   it('Vai renderizar um link para artigo no icone do Facebook', () => {
-  //     render(<Compartilhe pageType="artigo" slug="teste" />)
+    it('Vai renderizar um link para categorias no icone do Linkedin', () => {
+      render(
+        <Provider store={storePaginaCategoriasMock}>
+          <Compartilhe slug="teste" />
+        </Provider>
+      )
 
-  //     const FacebookIcon = screen.getByLabelText(/compartilhar no Facebook/i)
+      const LinkedinIcon = screen.getByLabelText(/compartilhar no Linkedin/i)
 
-  //     expect(FacebookIcon).toHaveAttribute(
-  //       'href',
-  //       `${API_FACEBOOK}${LINK_SITE}/artigo/teste`
-  //     )
-  //   })
-  // })
+      expect(LinkedinIcon).toHaveAttribute(
+        'href',
+        `${API_LINKEDIN}${LINK_SITE}/buscar?categorias=teste`
+      )
+    })
 
-  // describe('Linkedin icon', () => {
-  //   it('Vai renderizar um link para home no icone do Linkedin', () => {
-  //     render(<Compartilhe pageType="home" />)
+    it('Vai renderizar um link para tags no icone do Linkedin', () => {
+      render(
+        <Provider store={storePaginaTagsMock}>
+          <Compartilhe slug="teste" />
+        </Provider>
+      )
 
-  //     const LinkedinIcon = screen.getByLabelText(/compartilhar no Linkedin/i)
+      const LinkedinIcon = screen.getByLabelText(/compartilhar no Linkedin/i)
 
-  //     expect(LinkedinIcon).toHaveAttribute(
-  //       'href',
-  //       `${API_LINKEDIN}${LINK_SITE}`
-  //     )
-  //   })
+      expect(LinkedinIcon).toHaveAttribute(
+        'href',
+        `${API_LINKEDIN}${LINK_SITE}/buscar?tags=teste`
+      )
+    })
 
-  //   it('Vai renderizar um link para categorias no icone do Linkedin', () => {
-  //     render(<Compartilhe pageType="categorias" slug="teste" />)
+    it('Vai renderizar um link para artigo no icone do Linkedin', () => {
+      render(
+        <Provider store={storePaginaArtigoMock}>
+          <Compartilhe slug="teste" />
+        </Provider>
+      )
 
-  //     const LinkedinIcon = screen.getByLabelText(/compartilhar no Linkedin/i)
+      const LinkedinIcon = screen.getByLabelText(/compartilhar no Linkedin/i)
 
-  //     expect(LinkedinIcon).toHaveAttribute(
-  //       'href',
-  //       `${API_LINKEDIN}${LINK_SITE}/buscar?categorias=teste`
-  //     )
-  //   })
-
-  //   it('Vai renderizar um link para tags no icone do Linkedin', () => {
-  //     render(<Compartilhe pageType="tags" slug="teste" />)
-
-  //     const LinkedinIcon = screen.getByLabelText(/compartilhar no Linkedin/i)
-
-  //     expect(LinkedinIcon).toHaveAttribute(
-  //       'href',
-  //       `${API_LINKEDIN}${LINK_SITE}/buscar?tags=teste`
-  //     )
-  //   })
-
-  //   it('Vai renderizar um link para artigo no icone do Linkedin', () => {
-  //     render(<Compartilhe pageType="artigo" slug="teste" />)
-
-  //     const LinkedinIcon = screen.getByLabelText(/compartilhar no Linkedin/i)
-
-  //     expect(LinkedinIcon).toHaveAttribute(
-  //       'href',
-  //       `${API_LINKEDIN}${LINK_SITE}/artigo/teste`
-  //     )
-  //   })
-  // })
+      expect(LinkedinIcon).toHaveAttribute(
+        'href',
+        `${API_LINKEDIN}${LINK_SITE}/artigo/teste`
+      )
+    })
+  })
 })
